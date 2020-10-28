7523cc2d02b6c7bd9bce8852a87387fffb1ca169
pezzu <7243796+pezzu@users.noreply.github.com>
2020-10-28 18:47:37 -0400

    Tailwindcss added. Default stuff removed

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+    defaultLineHeights: true,
+    standardFontWeights: true
+  },
+  purge: [
+    './src/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+}
